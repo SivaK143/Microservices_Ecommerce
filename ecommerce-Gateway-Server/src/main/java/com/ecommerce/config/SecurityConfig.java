3c8c109e530dcae5b81e0d4fa3a4bdf8e4d1a9ee
siva <sivakrishnaboya45@gmail.com>
2026-05-12 10:38:09 +0530

    Role based access added

--- ecommerce-Gateway-Server/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ ecommerce-Gateway-Server/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ecommerce.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -23,6 +24,21 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> exchange
                         //Public APIs
                         .pathMatchers("/ecommerce/auth/**").permitAll()
+                        //product API's
+                        .pathMatchers(HttpMethod.GET, "/ecommerce/products/**").hasAnyRole("USER","ADMIN")
+                        .pathMatchers(HttpMethod.POST, "/ecommerce/products/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.PUT, "/ecommerce/products/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.DELETE, "/ecommerce/products/**").hasRole("ADMIN")
+
+                        // // CATEGORY APIs
+                        .pathMatchers(HttpMethod.GET,"/ecommerce/categories/**").hasAnyRole("ADMIN", "USER")
+                        .pathMatchers(HttpMethod.POST,"/ecommerce/categories/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.PUT, "/ecommerce/categories/**").hasRole("ADMIN")
+                         .pathMatchers(HttpMethod.DELETE, "/ecommerce/categories/**").hasRole("ADMIN")
+                        //Order APIS
+                        .pathMatchers(HttpMethod.POST, "/ecommerce/orders/**").hasAnyRole("USER","ADMIN")
+                        .pathMatchers(HttpMethod.GET, "/ecommerce/orders/My").hasRole("USER")
+                        .pathMatchers(HttpMethod.GET, "/ecommerce/orders").hasRole("ADMIN")
                         //All other are protected
                         .anyExchange().authenticated()
                 )
